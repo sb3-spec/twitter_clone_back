b2caa54e35a91a76c8f30965921e540eaca501cc
Robert Sheffield <sheffira2@gmail.com>
2022-03-15 17:41:00 -0700

    cors debug

--- twitter_clone_back/settings.py
+++ twitter_clone_back/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'no^e7hxhokj8j-o9nlk82()qst6k8-(+m!4w(w01mbm!&669tk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'twitter-clone-drf.herokuapp.com', 'dreamy-aryabhata-f7d882.netlify.app', 'dreamy-aryabhata-f7d882.netlify.app/']
 
