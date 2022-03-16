9ae4ca3c97750458999588b73054a421dc0a0144
Robert Sheffield <sheffira2@gmail.com>
2022-03-15 17:48:42 -0700

    cors debug

--- twitter_clone_back/settings.py
+++ twitter_clone_back/settings.py
@@ -64,8 +64,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
 ]
-CORS_ALLOW_ALL_ORIGIN = True
-CORS_ALLOWED_ORIGIN = [
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000',
     'http://localhost:3000',
     'http://localhost',
@@ -73,6 +73,7 @@ CORS_ALLOWED_ORIGIN = [
     'https://twitter-clone-drf.herokuapp.com/',
     'https://dreamy-aryabhata-f7d882.netlify.app'
 ]
+CORS_ALLOW_HEADERS = ['*']
 
 ROOT_URLCONF = 'twitter_clone_back.urls'
 
