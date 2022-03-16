7f40c54e9b739df8dd38cdbb31b767bc5f3b0b03
Robert Sheffield <sheffira2@gmail.com>
2022-03-15 18:06:39 -0700

    cors debug

--- twitter_clone_back/settings.py
+++ twitter_clone_back/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = ["*"]
 
 CORS_ALLOW_HEADERS = ['*']
